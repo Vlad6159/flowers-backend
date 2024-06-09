06ed8d4641d6c22dc644cd726e9c5aa3c689fde5
Vladislav Ilyushkin <ilyushkin.vlad@mail.ru>
2024-06-10 06:28:15 +0700

    12345

--- database/migrations/2024_06_09_232710_create_carts_table.php
+++ database/migrations/2024_06_09_232710_create_carts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('product_to_cart')->constrained('product_to_cards');
+            $table->foreignId('product_to_cart_id')->constrained('product_to_carts');
             $table->timestamps();
         });
     }
