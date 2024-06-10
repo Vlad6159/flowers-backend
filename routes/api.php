921a60946c0925da4b22302f0dfb5eece5935f3f
Vladislav Ilyushkin <ilyushkin.vlad@mail.ru>
2024-06-10 08:49:28 +0700

    изменения

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/data',[ProductsController::class,'getAllProductsAndCategories']);
 Route::post('/user/code', [UserController::class, 'createOrUpdateUserWithVerifyCode']);
 Route::post('/user/code/check', [UserController::class, 'verifyUserAndUpdateVerifyCode']);
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(CheckToken::class)->group(function () {
     // Маршрут /auth
     Route::get('/auth', function () {
         return response()->json(['message' => 'Authenticated']);
